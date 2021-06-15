45f88f13abea21c2c88cbb2c30fba0fdfe294541
Koldo Picaza <1093654+kpicaza@users.noreply.github.com>
2021-06-15 21:18:18 +0200

    functional example

--- fiber.php
+++ fiber.php
@@ -9,31 +9,54 @@ require 'vendor/autoload.php';
 
 ini_set('memory_limit', '-1');
 
+// Change to `true` to try multi-thread
+$multiThreads = false;
+
 $loop = new FiberLoop(Factory::create());
+$socket = new \React\Socket\Server('0.0.0.0:8080', $loop, [
+    'tcp' => [
+        'reuse_port' => true
+    ]
+]);
 
-$server = new \React\Http\Server(
-    $loop,
-    new \React\Http\Middleware\StreamingRequestMiddleware(),
-    new \React\Http\Middleware\LimitConcurrentRequestsMiddleware(512), // 100 concurrent buffering handlers
-    new \React\Http\Middleware\RequestBodyBufferMiddleware(1024), // 2 MiB per request
-    new \React\Http\Middleware\RequestBodyParserMiddleware(),
-    function (\Psr\Http\Message\ServerRequestInterface $request) use ($loop) {
-        return $loop->async(function () {
-            return new \React\Http\Message\Response(
-                200,
-                array(
-                    'Content-Type' => 'text/plain'
-                ),
-                "Hello World!\n"
-            );
+$serverInstance = static function () use ($loop, $socket) {
+    $server = new \React\Http\Server(
+        $loop,
+        new \React\Http\Middleware\StreamingRequestMiddleware(),
+        new \React\Http\Middleware\LimitConcurrentRequestsMiddleware(512), // 100 concurrent buffering handlers
+        new \React\Http\Middleware\RequestBodyBufferMiddleware(2 * 1024), // 2 MiB per request
+        new \React\Http\Middleware\RequestBodyParserMiddleware(),
+        function (\Psr\Http\Message\ServerRequestInterface $request) use ($loop) {
+            try {
+                return $loop->async(static function () use ($loop) {
+                    return $loop->await(new \React\Promise\Promise(function (callable $resolve) use ($loop) {
+                        $message = $loop->await(\React\Promise\resolve('Hola mundo'));
+
+                        $resolve(new \React\Http\Message\Response(
+                            200,
+                            array(
+                                'Content-Type' => 'application/json'
+                            ),
+                            json_encode([
+                                'message' => $message,
+                            ])
+                        ));
+
+                    }));
+                });
+            } catch (Throwable $exception) {
+                dump($exception);
+            }
         });
-    });
 
-$server->on('error', function (Throwable $exception) {
-    dump($exception);
-});
+    $server->listen($socket);
+};
 
-$socket = new \React\Socket\Server('0.0.0.0:8080', $loop);
-$server->listen($socket);
+if ($multiThreads) {
+    \Antidot\React\Child::fork(8, $serverInstance);
+} else {
+    $serverInstance();
+}
+
 
 $loop->run();
