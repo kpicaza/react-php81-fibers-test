24c6d55b7ccf2a47f14f55143f1e3672c359288f
Koldo Picaza <1093654+kpicaza@users.noreply.github.com>
2021-06-16 17:40:27 +0200

    add multi-thread suppor to server.php

--- server.php
+++ server.php
@@ -6,29 +6,46 @@ use React\EventLoop\Factory;
 
 require 'vendor/autoload.php';
 
+// Change to `true` to try multi-thread
+$multiThreads = true;
+
 $loop = Factory::create();
+$socket = new \React\Socket\Server('0.0.0.0:8080', $loop, [
+    'tcp' => [
+        'reuse_port' => true
+    ]
+]);
 
-$server = new \React\Http\Server(
-    $loop,
-    new \React\Http\Middleware\StreamingRequestMiddleware(),
-    new \React\Http\Middleware\LimitConcurrentRequestsMiddleware(512), // 100 concurrent buffering handlers
-    new \React\Http\Middleware\RequestBodyBufferMiddleware(1024), // 2 MiB per request
-    new \React\Http\Middleware\RequestBodyParserMiddleware(),
-    function (\Psr\Http\Message\ServerRequestInterface $request) use ($loop) {
-    return new \React\Http\Message\Response(
-        200,
-        array(
-            'Content-Type' => 'text/plain'
-        ),
-        "Hello World!\n"
-    );
-});
+$serverInstance = static function () use ($loop, $socket) {
 
-$server->on('error', function (Throwable $exception) {
-    dump($exception);
-});
+    $server = new \React\Http\Server(
+        $loop,
+        new \React\Http\Middleware\StreamingRequestMiddleware(),
+        new \React\Http\Middleware\LimitConcurrentRequestsMiddleware(512), // 100 concurrent buffering handlers
+        new \React\Http\Middleware\RequestBodyBufferMiddleware(1024), // 2 MiB per request
+        new \React\Http\Middleware\RequestBodyParserMiddleware(),
+        function (\Psr\Http\Message\ServerRequestInterface $request) use ($loop) {
+            return new \React\Http\Message\Response(
+                200,
+                array(
+                    'Content-Type' => 'text/plain'
+                ),
+                "Hello World!\n"
+            );
+        });
 
-$socket = new \React\Socket\Server('0.0.0.0:8080', $loop);
-$server->listen($socket);
+    $server->on('error', function (Throwable $exception) {
+        dump($exception);
+    });
+
+    $server->listen($socket);
+};
+
+if ($multiThreads) {
+    \Antidot\React\Child::fork(8, $serverInstance);
+} else {
+    $serverInstance();
+}
+
 
 $loop->run();
